40b5c1bdbbdce2b7278686005f44c8f8e66dbdd3
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-08-02 22:16:24 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    Basic annotations - floor
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+        Returns the floor  of number n.
+    """
+    return math.floor(n)
