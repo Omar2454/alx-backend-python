6830d5ca244f0c6601fbb7b37559d7f41bfefd28
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2024-08-02 22:36:39 +0300

    task

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """ mixed list """
-from typing import List
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """  returns sum as a float. """
     return float(sum(mxd_lst))
